1cc4cf80e5ce6f2303c20d8552fd9b13a4455fa3
DANIEL PEREIRA DE SOUZA <daniel.psouza11@senacsp.edu.br>
2024-10-21 16:21:53 -0300

    V- 1.0

---
+++ backup.bat
@@ -0,0 +1,4 @@
+mkdir backup
+copy \Windows\*.txt \Users\daniel.psouza11\documents\script-backup\backup
+copy \Windows\System32\*.log \Users\daniel.psouza11\documents\script-backup\backup
+copy \Windows\System32\*.txt \Users\daniel.psouza11\documents\script-backup\backup
